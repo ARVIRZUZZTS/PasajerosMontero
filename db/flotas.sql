27b018d0bde11b5968f15090c6bf0064566ccd7d
ArvirzuzztS <virzuzz12345@gmail.com>
2026-03-12 00:15:21 -0400

    base de datos de FLOTAS script

---
+++ db/flotas.sql
@@ -0,0 +1,18 @@
+INSERT INTO `flota`(`placa`, `chofer1`, `licencia1`, `chofer2`, `licencia2`, `asientos`, `tipo_bus`) VALUES 
+('3056-EAY', 'HUGO GABRIEL RASGUIDO', '8036400', '', '', 33, 'Semi-Cama' ),
+('1803-BN', '', '', '', '', 33, 'Semi-Cama'),
+('1800-FIU', 'MARIO CRUZ LACATO', '7995969', '', '', 33, 'Semi-Cama' ),
+('2537-DER', 'REINALDO JORGE OCZACHOQUE', '3531243', '', '', 33, 'Semi-Cama' ),
+('2996-UKF', 'JHONNY VARGAS HERBAS', '3780185', '', '', 33, 'Semi-Cama' ),
+('2447-CPE', 'ELOY TERCEROS MONTAÑO', '4512727', '', '', 33, 'Semi-Cama' ),
+('2494-RXU', 'CALIXTO ANDIA QUINTEROS', '2951649', '', '', 33, 'Semi-Cama' ),
+('2550-TFU', 'DEMETRIO GALINDO MERIDA', '4438619', '', '', 33, 'Semi-Cama' ),
+('1194-UKE', 'ERICK MENDOZA MONTESINOS', '6472193', '', '', 33, 'Semi-Cama' ),
+('2830-UTA', 'ARIEL VARGAS VALLEJOS', '7939232', '', '', 33, 'Semi-Cama' ),
+('1461-KUX', 'WILSON LAZARTE MONTAÑO', '8691695', '', '', 33, 'Semi-Cama' ),
+('1580-EYR', 'JUAN QUISPE LLAMPAS', '4321568', '', '', 33, 'Semi-Cama' ),
+('2130-YXG', 'JHONNY VARGAS', '6053938', '', '', 33, 'Semi-Cama'),
+('2447-DKT', 'RICHAR IRUSTA JIMENEZ', '4560090', '', '', 33, 'Semi-Cama'),
+('2701-YNF', 'ARCENIO CABALLERO V.', '3567742', '', '', 33, 'Semi-Cama')
+('2264-KGD', 'VICTOR HUGO VELARSCO CAERO', '4403658', '', '', 33, 'Semi-Cama'),
+('2218-PCT', 'ALEX SANDRO VILLARROEL ROMERO', '4512363', '', '', 33, 'Semi-Cama')
