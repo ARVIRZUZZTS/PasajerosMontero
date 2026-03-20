8c8e0dcf67db982aa08e02312e2aa6694db5ab28
ArvirzuzztS <virzuzz12345@gmail.com>
2026-03-20 00:29:43 -0400

    Ignorando conexion.php

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conexion = mysqli_connect("localhost","root","","pasajeromontero");
-# en el tercer "" colocar el puerto "3307"-Waka; ""-Arvi
-if(!$conexion){
-    die("Error de conexión: " . mysqli_connect_error());
-}
-#echo "Conectado correctamente a la base de datos";
-?>
